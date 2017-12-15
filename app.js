2577554bcd2fd50f3a64c31d4c0232278a6b589d
Ryan Palmer <ryan.palm93@gmail.com>
2017-12-14 16:52:05 -0800

    form added to HTML and JS, inputs not creating new row in table.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm',
 '4pm', '5pm', '6pm', '7pm'];
 
@@ -8,7 +9,10 @@ function random(min, max) {
 }
 
 var allStores = [];
+var form = document.getElementById('store-sales');
 var storeTable = document.getElementById('store-sales');
+// var data = [];
+
 
 function Store(name, minCustPerHour, maxCustPerHour, avgCookiePerCust) {
   this.name= name;
@@ -59,6 +63,34 @@ function Store(name, minCustPerHour, maxCustPerHour, avgCookiePerCust) {
   new Store('Capitol Hill', 20, 38, 2.3);
   new Store('Alki', 2, 16, 4.6);
 
+function formData(event) {
+  event.preventDefault();
+
+  var name = event.target.store.value;
+  var minCust = event.target.minCust.value;
+  var maxCust = event.target.maxCust.value;
+  var avgCookie = event.target.avgCookie.value;
+
+  allStores.push(new Store(name, minCustPerHour, maxCustPerHour, avgCookiePerCust));
+  // createTable();
+  form.reset();
+  allStores[5].render();
+}
+
+// function createTable() {
+//   var row;
+//   for (var i = 0; i < allStores.length; i++) {
+//     row = document.createElement('tr');
+//     row.innerHTML =
+//       '<td>' + data[i].store + '</td>' +
+//        '<td>' + data[i].minCust + '</td>' +
+//        '<td>' + data[i].maxCust + '</td>' +
+//        '<td>' + data[i].avgCookie + '</td>'
+//  }
+//
+//  table.appendChild(row);
+// }
+
   function renderHeaderRow() {
     var trEl = document.createElement('tr');
     var tdEl = document.createElement('td');
@@ -78,6 +110,8 @@ function Store(name, minCustPerHour, maxCustPerHour, avgCookiePerCust) {
       allStores[i].render();
     };
   }
+
+form.addEventListener('submit', formData);
 
 renderHeaderRow();
 renderStoreRows();
